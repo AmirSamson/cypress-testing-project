9affe49023c468b5c51eb5f830c4de85a4b24446
Amir Samson <amirhsamson93@gmail.com>
2025-05-19 21:45:27 +0330

    added the assertions

--- cypress/e2e/TC-0001.spec.cy.js
+++ cypress/e2e/TC-0001.spec.cy.js
@@ -1,15 +1,14 @@
 describe('Google Search Tests', () => {
   it('types into the search bar and verifies the input value', () => {
-    cy.visit('https://www.google.com');
+    cy.visit('https://duckduckgo.com');
     
-    cy.get('#APjFqb')
+    cy.get('#searchbox_input')
       .type('Cypress testing')
       .should('have.value', 'Cypress testing') 
       .and('be.visible') 
       .type('{enter}');
-    cy.waitfortimeout(2000);
-    cy.get('[style="width: 304px; height: 78px;"] > div > iframe').click();
-    cy.url().should('include', 'search?q=Cypress+testing');
-    cy.get('#search').should('exist').and('be.visible');
+
+    cy.url().should('include', '=Cypress+testing');
+    cy.get('#searchbox_input').should('exist').and('be.visible');
   });
 });
