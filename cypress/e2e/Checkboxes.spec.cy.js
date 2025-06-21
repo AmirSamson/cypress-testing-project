7dd822f1df6456dca49c3e4c055e8e28d73e7bce
Amir Samson <amirhsamson93@gmail.com>
2025-06-21 12:25:02 +0330

    added the checkbox assertion

--- cypress/e2e/Checkboxes.spec.cy.js
+++ cypress/e2e/Checkboxes.spec.cy.js
@@ -2,7 +2,7 @@ describe('checkboxes tic', () => {
    it('should be checked', () => {
     cy.visit('https://the-internet.herokuapp.com/checkboxes');
     cy.get('#checkboxes > :nth-child(1)').click();
-    cy.get('#checkboxes > :nth-child(1)').should(d)
-    cy.asser
+    cy.get('#checkboxes > :nth-child(1)').check();
+    
    })
 })
